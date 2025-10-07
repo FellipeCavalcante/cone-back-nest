4ad88d386d2f6795c97148bd3c568b56eb4effbb
FellipeCavalcante <azevedoaranha1@gmail.com>
2025-10-07 20:58:11 -0300

    new controller router

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
-import { UserService } from './user.service';
+import { Controller, Get } from "@nestjs/common";
+import { UserService } from "./user.service";
+import { GetUser } from "src/utils/decorators/get-user.decorator";
+import { UserDomain } from "./domain/user";
 
-@Controller('api/v2/user')
+@Controller("api/v2/user")
 export class UserController {
   constructor(private service: UserService) {}
 
-  @Get('reports')
+  @Get("reports")
   async reports() {
     return this.service.reports();
+  }
+
+  @Get("refresh-token")
+  async refresh(@GetUser() user: UserDomain) {
+    return this.service.refreshUsers(user);
   }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,9 +1,35 @@
 import { Injectable, InternalServerErrorException } from "@nestjs/common";
 import { PrismaService } from "src/config/database/prisma.service";
+import { UserDomain } from "./domain/user";
+import { JwtService } from "@nestjs/jwt";
 
 @Injectable()
 export class UserService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
+
+  async refreshUsers(user: UserDomain): Promise<{ access_token: string }> {
+    const userInDb = await this.prisma.users.findUnique({
+      where: { id: user.id },
+    });
+
+    if (!userInDb) {
+      throw new InternalServerErrorException("User not found");
+    }
+
+    const payload = {
+      sub: userInDb.id,
+      email: userInDb.email,
+      type: userInDb.type,
+      enterpriseId: userInDb.enterprise_id,
+      subSectorId: userInDb.sub_sector_id,
+    };
+    const token = await this.jwtService.signAsync(payload);
+
+    return { access_token: token };
+  }
 
   async reports(page = 1, pageSize = 10) {
     try {
