95039f8d2ddf8868880969ec16929a5d38bb7089
FellipeCavalcante <azevedoaranha1@gmail.com>
2025-10-07 20:58:01 -0300

    remove

--- src/utils/filters/verify-user-type.ts
+++
@@ -1,8 +0,0 @@
-export async function verifyUserType(userId: string) {
-  const user = await this.prisma.users.findUnique({
-    where: { id: userId },
-    select: { type: true },
-  });
-
-  return user.type;
-}
