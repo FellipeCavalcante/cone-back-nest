7b0067b4a779837be44e4b64470950cb9cbd8851
FellipeCavalcante <azevedoaranha1@gmail.com>
2025-10-13 15:48:09 -0300

    update sub sector controller

--- src/modules/subSector/subSecotr.controller.ts
+++ src/modules/subSector/subSecotr.controller.ts
@@ -1,15 +1,26 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+} from "@nestjs/common";
 import { CreateSubSectorDto } from "./dtos/create-subSector";
 import { UserDomain } from "../user/domain/user";
 import { GetUser } from "src/utils/decorators/get-user.decorator";
 import { CreateSubSectorUseCase } from "./use-cases/create-subSector";
 import { AddUserInSubSectorUseCase } from "./use-cases/add-user";
+import { UpdateSubSectorUseCase } from "./use-cases/update-subSector";
+import { UpdateSubSectorDto } from "./dtos/update-subSector";
 
 @Controller("api/v2/subSector")
 export class SubSectorController {
   constructor(
     private createSubSectorUseCase: CreateSubSectorUseCase,
     private addUserUseCase: AddUserInSubSectorUseCase,
+    private updateSubSectorUseCase: UpdateSubSectorUseCase,
   ) {}
 
   @Post("create")
@@ -21,16 +32,22 @@ export class SubSectorController {
     return this.createSubSectorUseCase.execute({ user, ...request });
   }
 
-  @Post("add-users")
+  @Post("add-users/:subSectorId")
   @HttpCode(HttpStatus.OK)
   async addUsers(
     @GetUser() user: UserDomain,
-    @Body() request: { subSectorId: string; memberId: string },
+    @Body() request: { memberId: string },
+    @Param("subSectorId") subSectorId: string,
   ) {
-    return this.addUserUseCase.execute(
-      user,
-      request.subSectorId,
-      request.memberId,
-    );
+    return this.addUserUseCase.execute(user, subSectorId, request.memberId);
+  }
+
+  @Patch("update/:subSectorId")
+  @HttpCode(HttpStatus.OK)
+  async update(
+    @Body() request: UpdateSubSectorDto,
+    @Param("subSectorId") subSectorId: string,
+  ) {
+    return this.updateSubSectorUseCase.execute(subSectorId, request.name);
   }
 }
